7514026e33ec6deef54fac17a613e15108a0a0b9
Ciel248 <xxxcielxxx@gmail.com>
2015-02-27 22:37:43 +0900

    デフォルトでコメントアウトされているgemでよく使うものを追加

---
+++ template.rb
@@ -0,0 +1,12 @@
+# Gemfile
+append_file "Gemfile", <<-EGF
+
+# See https://github.com/sstephenson/execjs#readme for more supported runtimes
+gem 'therubyracer', platforms: :ruby
+
+# Use Unicorn as the app server
+gem 'unicorn'
+
+# Use Capistrano for deployment
+gem 'capistrano-rails', group: :development
+EGF
