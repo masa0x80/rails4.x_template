e606c33b161c93b18f6753fe208c5148043030bb
Kimura Masayuki <masa0x80@gmail.com>
2015-10-14 01:09:52 +0900

    Small fix

--- template.rb
+++ template.rb
@@ -1,11 +1,11 @@
-@database = options["database"]
+@database = options['database']
 
 # git init
 git :init
-git add: "."
+git add: '.'
 git commit: "-m '[command] rails new #{app_name} -d #{@database}'"
 
-run "mv config/database.yml config/database.yml.tmpl"
+run 'mv config/database.yml config/database.yml.tmpl'
 
 # .gitignore
 run "cat <<-EGI >> .gitignore
@@ -16,7 +16,7 @@ config/application.yml
 vendor/bundle
 EGI"
 
-git add: "-A"
+git add: '.'
 git commit: "-m 'config/database.ymlをgitの管理外に'"
 
 run "cp config/database.yml.tmpl config/database.yml"
@@ -32,41 +32,41 @@ gem 'unicorn'
 
 # Use Capistrano for deployment
 gem 'capistrano-rails',    group: :development
-gem "capistrano-rbenv",    group: :development
-gem "capistrano-bundler",  group: :development
-gem "capistrano3-unicorn", group: :development
+gem 'capistrano-rbenv',    group: :development
+gem 'capistrano-bundler',  group: :development
+gem 'capistrano3-unicorn', group: :development
 
 # settingslogic
-gem "settingslogic"
+gem 'settingslogic'
 
 # slim-rails
-gem "slim-rails"
+gem 'slim-rails'
 
 # pry
-gem "pry-rails",  group: [:development, :test]
-gem "pry-byebug", group: [:development, :test]
+gem 'pry-rails',  group: [:development, :test]
+gem 'pry-byebug', group: [:development, :test]
 
 # rspec
-gem "rspec-rails", group: [:development, :test]
+gem 'rspec-rails', group: [:development, :test]
 
 # factory_girl
-gem "factory_girl_rails"
+gem 'factory_girl_rails'
 
 # annotate
-gem "annotate", group: :development
+gem 'annotate', group: :development
 
 # bullet
-gem "bullet", group: :development
+gem 'bullet', group: :development
 EGF
 
-file "app/models/settings.rb", <<-'EOC'
+file 'app/models/settings.rb', <<-'EOC'
 class Settings < Settingslogic
   source "#{Rails.root}/config/application.yml"
   namespace Rails.env
 end
 EOC
 
-file "config/application.yml.tmpl", <<-'EOC'
+file 'config/application.yml.tmpl', <<-'EOC'
 defaults: &defaults
 
 development:
@@ -77,179 +77,178 @@ production:
   <<: *defaults
 EOC
 
-run "cp config/database.yml config/database.yml.tmpl"
-run "cp config/application.yml.tmpl config/application.yml"
+run 'cp config/database.yml config/database.yml.tmpl'
+run 'cp config/application.yml.tmpl config/application.yml'
 
-run "bundle install --path=vendor/bundle --jobs=4"
-run "bundle package"
-git add: "."
+run 'bundle install --path=vendor/bundle --jobs=4'
+run 'bundle package'
+git add: '.'
 git commit: "-m '[command] bundle instal --path=vendor/bundle; bundle package'"
 
 after_bundle do
-  run "bundle exec cap install"
-  git add: "."
+  run 'bundle exec cap install'
+  git add: '.'
   git commit: "-m '[command] bundle exec cap install'"
 
-  gsub_file "Capfile", "# require 'capistrano/rbenv'", "require 'capistrano/rbenv'"
-  gsub_file "Capfile", "# require 'capistrano/bundler'", "require 'capistrano/bundler'"
-  inject_into_file "config/deploy.rb", after: "# set :keep_releases, 5\n" do
+  gsub_file 'Capfile', '# require 'capistrano/rbenv'', 'require 'capistrano/rbenv''
+  gsub_file 'Capfile', '# require 'capistrano/bundler'', 'require 'capistrano/bundler''
+  inject_into_file 'config/deploy.rb', after: "# set :keep_releases, 5\n" do
     <<-CODE.strip_heredoc
-      
       # skip capistrano stats
       Rake::Task['metrics:collect'].clear_actions
     CODE
   end
-  gsub_file "config/deploy.rb", "# set :keep_releases, 5", "set :keep_releases, 5"
+  gsub_file 'config/deploy.rb', '# set :keep_releases, 5', 'set :keep_releases, 5'
   git add: "."
   git commit: "-m 'updaet capistrano settings'"
 
-  run "bundle exec rails g rspec:install"
-  append_file ".rspec", "--format documentation"
-  git add: "."
+  run 'bundle exec rails g rspec:install'
+  append_file '.rspec', '--format documentation'
+  git add: '.'
   git commit: "-m '[command] bundle exec rails g rspec:install'"
 
-  rakefile("auto_annotate.rake") do
+  rakefile('auto_annotate.rake') do
     <<-TASK.strip_heredoc
       task :annotate do
-        puts "Annotating models..."
-        system "bundle exec annotate"
+        puts 'Annotating models...'
+        system 'bundle exec annotate'
       end
 
-      if Rails.env == "development"
-        Rake::Task["db:migrate"].enhance do
-          Rake::Task["annotate"].invoke
+      if Rails.env.to_sym == :development
+        Rake::Task['db:migrate'].enhance do
+          Rake::Task['annotate'].invoke
         end
 
-        Rake::Task["db:rollback"].enhance do
-          Rake::Task["annotate"].invoke
+        Rake::Task['db:rollback'].enhance do
+          Rake::Task['annotate'].invoke
         end
       end
     TASK
   end
-  git add: "."
+  git add: '.'
   git commit: "-m 'settings for annotate automatically'"
 
-  if yes?("use devise?")
-    append_file "Gemfile", <<-EOG.strip_heredoc
+  if yes?('Use devise?')
+    append_file 'Gemfile', <<-EOG.strip_heredoc
       
       # devise
-      gem "devise"
+      gem 'devise'
     EOG
 
-    run "bundle install"
-    git add: "."
+    run 'bundle install'
+    git add: '.'
     git commit: "-m '[gem] devise'"
 
-    if yes?("generate with basic option?")
-      generate "devise:install"
-      git add: "."
+    if yes?('Generate with basic option?')
+      generate 'devise:install'
+      git add: '.'
       git commit: "-m '[command] bundle exec rails g devise:install'"
 
-      generate "devise", "user"
-      git add: "."
+      generate 'devise', 'user'
+      git add: '.'
       git commit: "-m '[command] bundle exec rails g devise user'"
 
-      rake "db:create"
-      rake "db:migrate"
-      git add: "."
+      rake 'db:create'
+      rake 'db:migrate'
+      git add: '.'
       git commit: "-m '[command] bundle exec rake db:migrate'"
 
-      inject_into_file "app/controllers/application_controller.rb", after: "protect_from_forgery with: :exception\n" do
+      inject_into_file 'app/controllers/application_controller.rb', after: "protect_from_forgery with: :exception\n" do
         "  before_action :authenticate_user!\n"
       end
       environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: :development
       route "root to: 'top#index'"
-      file "app/controllers/top_controller.rb", <<-CODE.strip_heredoc
+      file 'app/controllers/top_controller.rb', <<-CODE.strip_heredoc
         class TopController < ApplicationController
           def index
           end
         end
       CODE
-      file "app/views/top/index.html.slim", <<-CODE.strip_heredoc
+      file 'app/views/top/index.html.slim', <<-CODE.strip_heredoc
         h1 Top Page
       CODE
 
-      git add: "."
+      git add: '.'
       git commit: "-m 'generate for devise with basic parameters'"
     end
   end
 
-  if yes?("use omniauth?")
-    append_file "Gemfile", <<-EOG.strip_heredoc
-      
+  if yes?('Use omniauth?')
+    append_file 'Gemfile', <<-EOG.strip_heredoc
+
       # omniauth
-      gem "omniauth-oauth2"
+      gem 'omniauth-oauth2'
     EOG
 
-    run "bundle install"
-    git add: "."
+    run 'bundle install'
+    git add: '.'
     git commit: "-m '[gem] omniauth-oauth2'"
   end
 
-  if yes?("use bootstrap?")
-    append_file "Gemfile", <<-EOG.strip_heredoc
-      
-      # bootstrap
-      gem "bootstrap-sass"
-      gem "bootstrap-sass-extras"
-      gem "momentjs-rails"
-      gem "bootstrap3-datetimepicker-rails"
-    EOG
-    run "bundle install"
+  if yes?('Use bootstrap?')
+    append_file 'Gemfile', <<-EOG.strip_heredoc
 
-    git add: "."
+      # bootstrap
+      gem 'bootstrap-sass'
+      gem 'bootstrap-sass-extras'
+      gem 'momentjs-rails'
+      gem 'bootstrap3-datetimepicker-rails'
+    EOG
+    run 'bundle install'
+
+    git add: '.'
     git commit: "-m '[gem] bootstrap-sass, bootstrap-sass-extras, bootstrap-datetimepicker'"
 
-    generate "bootstrap:install"
-    git add: "."
+    generate 'bootstrap:install'
+    git add: '.'
     git commit: "-m '[command] bundle exec rails g bootstrap:install'"
 
-    generate "bootstrap:layout", "application", "fluid"
-    git add: "."
+    generate 'bootstrap:layout', 'application', 'fluid'
+    git add: '.'
     git commit: "-m '[command] bundle exec rails g bootstrap:layout application fluid'"
 
-    run "rm app/views/layouts/application.html.erb"
-    run "mv app/assets/stylesheets/application.css app/assets/stylesheets/application.scss"
-    append_file "app/assets/stylesheets/application.scss", <<-CODE.strip_heredoc
-      @import "bootstrap-sprockets";
-      @import "bootstrap";
-      @import "bootstrap-datetimepicker";
+    run 'rm app/views/layouts/application.html.erb'
+    run 'mv app/assets/stylesheets/application.css app/assets/stylesheets/application.scss'
+    append_file 'app/assets/stylesheets/application.scss', <<-CODE.strip_heredoc
+      @import 'bootstrap-sprockets';
+      @import 'bootstrap';
+      @import 'bootstrap-datetimepicker';
 
       body {
         padding: 65px;
       }
     CODE
 
-    inject_into_file "app/assets/javascripts/application.js", after: "//= require jquery_ujs\n" do
+    inject_into_file 'app/assets/javascripts/application.js', after: "//= require jquery_ujs\n" do
       <<-CODE.strip_heredoc
         //= require bootstrap-sprockets
         //= require moment
         //= require bootstrap-datetimepicker
       CODE
     end
-    git add: "-A"
+    git add: '.'
     git commit: "-m 'add settings for bootstrap'"
   end
 
-  if yes?("use kaminari?")
-    append_file "Gemfile", <<-EGF.strip_heredoc
-      
+  if yes?('Use kaminari?')
+    append_file 'Gemfile', <<-EGF.strip_heredoc
+
       # kaminari
-      gem "kaminari"
+      gem 'kaminari'
     EGF
 
-    run "bundle install"
-    git add: "."
+    run 'bundle install'
+    git add: '.'
     git commit: "-m '[gem] kaminari'"
 
-    generate "kaminari:config"
-    git add: "."
+    generate 'kaminari:config'
+    git add: '.'
     git commit: "-m '[command] bundle exec rails g kaminari:config'"
 
-    kaminari_theme = ask("which theme of kaminari? [none|bootstrap3|foundation|github|google|purecss|semantic_ui]")
-    unless kaminari_theme == "none"
-      generate "kaminari:view", kaminari_theme
-      git add: "."
+    kaminari_theme = ask('Which theme of kaminari? [none|bootstrap3|foundation|github|google|purecss|semantic_ui]')
+    unless kaminari_theme == 'none'
+      generate 'kaminari:view', kaminari_theme
+      git add: '.'
       git commit: "-m '[command] bundle exec rails g kaminari:view #{kaminari_theme}'"
     end
   end
